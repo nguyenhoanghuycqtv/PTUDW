eec5587bf15fc1d5dc8ed2041cfa51f2ea2e2c20
nguyenhoanghuycqtv <nguyenhoanghuycqtv@gmail.com>
2023-02-15 14:27:47 +0700

    app/api-error.js

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+exports.create = (req, res, next) => {
+    res.send({ message: 'create handler' })
+}
+exports.findAll = (req, res, next) => {
+    res.send({ message: 'findAll handler' })
+}
+exports.findOne = (req, res, next) => {
+    res.send({ message: 'findOne handler' })
+}
+exports.update = (req, res, next) => {
+    res.send({ message: 'update handler' })
+}
+exports.delete = (req, res, next) => {
+    res.send({ message: 'delete handler' })
+}
+exports.deleteAll = (req, res, next) => {
+    res.send({ message: 'deleteAll handler' })
+}
+exports.findAllFavorite = (req, res, next) => {
+    res.send({ message: 'findAll handler' })
+}
